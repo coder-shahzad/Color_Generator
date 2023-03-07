13b3542478e70cd9510e29e84f9be1272ec07c21
Shahzad Gul Khan <108107290+coder-shahzad@users.noreply.github.com>
2023-03-07 09:04:59 +0530

    Add files via upload
    Complete color generator

---
+++ main.js
@@ -0,0 +1,11 @@
+const getcolor = () => {
+  // hex code
+  const randomNumber =Math.floor( Math.random()*16777215);
+  const randomCode = "#" + randomNumber.toString(16);
+  document.body.style.background = randomCode;
+  document.getElementById("color-code").innerText = randomCode;
+};
+
+document.getElementById("btn").addEventListener("click", getcolor);
+
+navigator.clipboard.writeText(randomCode)
